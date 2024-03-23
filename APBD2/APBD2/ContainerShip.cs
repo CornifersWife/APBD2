06fb4f2b93dba910c21363876ea835b867590dde
CornifersWife <irys.latosz@gmail.com>
2024-03-23 14:24:17 +0100

    Update ContainerShip.cs

--- APBD2/APBD2/ContainerShip.cs
+++ APBD2/APBD2/ContainerShip.cs
@@ -1,59 +1,79 @@
 namespace APBD2;
 
 public class ContainerShip {
-    private List<Container> Containers { get; set; }
+    private List<Container?> Containers { get; set; }
     private float maxSpeed; //in knots
     private int maxContainerAmount;
     private float maxTotalContainersLoad; //in tons
+    private float currentTotalContainerLoad = 0f;
 
     public ContainerShip(float maxSpeed, int maxContainerAmount, float maxTotalContainersLoad) {
         this.maxSpeed = maxSpeed;
         this.maxContainerAmount = maxContainerAmount;
         this.maxTotalContainersLoad = maxTotalContainersLoad;
-        Containers = new List<Container>();
+        Containers = new List<Container?>();
     }
 
     public ContainerShip(float maxSpeed, int maxContainerAmount, float maxTotalContainersLoad,
-        List<Container> containers) {
+        List<Container?> containers) {
         this.maxSpeed = maxSpeed;
         this.maxContainerAmount = maxContainerAmount;
         this.maxTotalContainersLoad = maxTotalContainersLoad;
         Containers = containers;
     }
 
-    public void RemoveContainer(Container container) {
+    public void RemoveContainer(Container? container) {
+        //TOOD remove load
         Containers.Remove(container);
     }
 
     public void AddContainers(List<Container> addedContainers) {
-        //check for too many containers
-        //check for maxtotalload
         foreach (Container addedContainer in addedContainers) {
-            Containers.Add(addedContainer);
+            AddContainer(addedContainer);
         }
     }
 
-    public void AddContainers(Container addedContainer) {
-        //check for maxtotalload
-        //check for too many containers
+    public void AddContainer(Container addedContainer) {
+        if (Containers.Count >= maxContainerAmount) {
+            throw new Exception("Max container amount has been exceeded on this ship");
+        }
+
+        float totalContainerMass = addedContainer.containerMass + addedContainer.load;
+        if (totalContainerMass + currentTotalContainerLoad > maxTotalContainersLoad) {
+            throw new Exception("Max total container has been exceeded on this ship");
+        }
+
         Containers.Add(addedContainer);
     }
 
-    public Container FindContainer(string containerSerialNumber) {
-        return null;
+    public Container? FindContainer(string containerSerialNumber) {
+        Container? container = Containers.Find(container => container.SerialNumber == containerSerialNumber);
+        return container;
     }
 
     public void ChangeContainer(Container newContainer, string oldContainerSerialNumber) {
-        Container oldContainer = FindContainer(oldContainerSerialNumber);
+        Container? oldContainer = FindContainer(oldContainerSerialNumber);
+        //TODO finish implementing
+    }
+
+    public static void MoveContainerBetweenShips(String containerSerialNumber, ContainerShip from, ContainerShip to) {
+        Container container = from.FindContainer(containerSerialNumber) ?? throw new InvalidOperationException();
+        MoveContainerBetweenShips(container, from, to);
     }
 
     public static void MoveContainerBetweenShips(Container container, ContainerShip from, ContainerShip to) {
+        if (!from.Containers.Contains(container)) {
+            throw new Exception($"{container.SerialNumber} is not on {from}");
+        }
+
+        from.RemoveContainer(container);
+        to.AddContainer(container);
     }
 
 
     public void GetInformation() {
         float currentLoad = 0f;
-        foreach (Container container in Containers) {
+        foreach (Container? container in Containers) {
             currentLoad += container.load;
         }
 
@@ -63,4 +83,5 @@ public class ContainerShip {
                           $"Containers: {Containers.Count}/{maxContainerAmount}" +
                           $"Current load: {currentLoad}/{maxTotalContainersLoad} tons");
     }
+    //TODO check for total weight of containers whenever load is added to any container
 }
